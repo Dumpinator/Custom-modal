9b5d0ad54ef1ad2f980b91dc1f61f68f783465e7
Dumpinator <jdeboisvilliers@gmail.com>
2019-03-22 16:48:05 +0100

    1er commit
    add html, css and js

---
+++ modal.js
@@ -0,0 +1,41 @@
+let modal = null
+
+const openModal = function (e) {
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display = null
+    target.removeAttribute('aria-hidden')
+    target.setAttribute('aria-modal', 'true')
+    modal = target
+    modal.addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation)
+}
+
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault()
+    modal.style.display = "none"
+    modal.setAttribute('aria-hidden', 'true')
+    modal.setAttribute('aria-modal', 'false')
+    modal.removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal)
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
+    modal = null
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation()
+}
+
+// Pour évitez que la modal se ferme quand on click à l'intérrieur
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal) 
+})
+
+window.addEventListener('keydown', function (e) {
+    // Si l'utilisateur appui sur Echap on ferme la modal
+    if (e.key === "Escape" || e.key ==="Esc") {
+        closeModal(e)
+    }
+})
